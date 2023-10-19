45921d54bce5fd74c66e367468933fe3b94888ee
Prositkd <prosit.kd@gmail.com>
2023-10-19 22:48:00 +0600

    Initial commit

---
+++ lab_task/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using lab_task.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_task.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var db = new LabTaskEntities2();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Register(int customerId, int[] Products)
+        {
+            var db = new LabTaskEntities2();
+            foreach (var p in Products)
+            {
+                var cs = new Order()
+                {
+                    Cid = customerId,
+                    Pid = p,
+                    Time= DateTime.Now,
+                };
+                db.Orders.Add(cs);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Login(int Id)
+        {
+            var db = new LabTaskEntities2();
+           // var data = db.Products.Find(id);
+            var data = (from d in db.Customers
+                    where d.Id == Id
+                      select d).SingleOrDefault();
+            return View(data);
+        }
+
+    }
+}
---
+++ lab_task/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab_task.EF;
+
+namespace lab_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new LabTaskEntities2();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new LabTaskEntities2();
+            var data = db.Products.Find(id);
+            //var data = (from d in db.Departments
+            //            where d.Id == id
+            //            select d).SingleOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product d)
+        {
+            var db = new LabTaskEntities2();
+            var ex = db.Products.Find(d.Id);
+            ex.pName = d.pName;
+             ex.price = d.price;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var db = new LabTaskEntities2();
+            var ex = db.Products.Find(id);
+            db.Products.Remove(ex);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product d)
+        {
+            var db = new LabTaskEntities2();
+            db.Products.Add(d);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
